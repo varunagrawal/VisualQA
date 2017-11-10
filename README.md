be9b1b0f27991ad8d63420dc9f4c986f747fe383
Varun Agrawal <varagrawal@gmail.com>
2017-11-10 07:21:32 -0500

    README update

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # VQA
 
 Visual Question Answering in PyTorch
+
+
+## Setup
+
+We use Pipenv since it is the Python recommended way of managing dependencies.
+
+- Create your virtual environment and install all your dependencies with
+
+    `pipenv install`
+
+- Download the VQA (2.0) dataset from [visualqa.org](http://visualqa.org/)
+- I have already pre-processed all the COCO images using VGG-16. You can find them [here]().
+
+
+## Running
+
+To run the code, just type `python main.py <options>`.
+
+You can get a list of options with `python main.py -h`.
--- main.py
+++ main.py
@@ -6,10 +6,15 @@ import dataset
 def parse_args():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--annotations", metavar="ANN", default="~/datasets/VQA2/v2_mscoco_train2014_annotations.json")
-    parser.add_argument("--questions", metavar="QUES", default="~/datasets/VQA2/v2_OpenEnded_mscoco_train2014_questions.json")
-    parser.add_argument("--images", default="coco_train_vgg_fc7.pth")
-    parser.add_argument("--top_answer_limit", default=1000)
+    parser.add_argument("--annotations", metavar="ANN",
+                        default="~/datasets/VQA2/v2_mscoco_train2014_annotations.json",
+                        help="The VQA annotations JSON file")
+    parser.add_argument("--questions", metavar="QUES",
+                        default="~/datasets/VQA2/v2_OpenEnded_mscoco_train2014_questions.json",
+                        help="The VQA questions JSON file")
+    parser.add_argument("--images", default="coco_train_vgg_fc7.pth",
+                        help="The file containing torch tensors of the FC7 embeddings of COCO images")
+    parser.add_argument("--top_answer_limit", default=1000, help="The number of answers to consider as viable options")
     parser.add_argument("--max_length", default=25, help="The maximum length to consider to each question")
     parser.add_argument("--batch_size", default=32)
     parser.add_argument('-j', "--num_workers", default=4)
