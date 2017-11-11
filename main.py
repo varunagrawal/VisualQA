19d8f2720fd029428bd8fc55698009e62ca960fb
Varun Agrawal <varagrawal@gmail.com>
2017-11-11 02:22:21 -0500

    nice formatting

--- main.py
+++ main.py
@@ -17,9 +17,17 @@ def parse_args():
     parser.add_argument("--questions", metavar="QUES",
                         default="~/datasets/VQA2/v2_OpenEnded_mscoco_train2014_questions.json",
                         help="The VQA questions JSON file")
-    parser.add_argument("--embed_question", action="store_true")
     parser.add_argument("--images", default="coco_train_vgg_fc7.pth",
                         help="The file containing torch tensors of the FC7 embeddings of COCO images")
+    parser.add_argument("--val-annotations", metavar="ANN",
+                        default="~/datasets/VQA2/v2_mscoco_val2014_annotations.json",
+                        help="The VQA val annotations JSON file")
+    parser.add_argument("--val-questions", metavar="QUES",
+                        default="~/datasets/VQA2/v2_OpenEnded_mscoco_val2014_questions.json",
+                        help="The VQA val questions JSON file")
+    parser.add_argument("--val-images", default="coco_val_vgg_fc7.pth",
+                        help="The file containing torch tensors of the FC7 embeddings of val COCO images")
+    parser.add_argument("--embed_question", action="store_true")
     parser.add_argument("--top_answer_limit", default=1000, help="The number of answers to consider as viable options")
     parser.add_argument("--max_length", default=25, help="The maximum length to consider to each question")
     parser.add_argument("--epochs", default=50, help="Number of training epochs")
@@ -40,14 +48,11 @@ def main():
     questions = osp.expanduser(args.questions)
 
     vqa_loader = dataset.get_dataloader(annotations, questions, args.images, "train", args)
-    # TODO parameterize the val_loader
-    val_loader = dataset.get_dataloader(
-        "/home/varun/datasets/VQA2/v2_mscoco_val2014_annotations.json",
-        "/home/varun/datasets/VQA2/v2_OpenEnded_mscoco_val2014_questions.json",
-        "coco_val_vgg_fc7.pth",
-        "val",
-        args
-    )
+
+    val_loader = dataset.get_dataloader(osp.expanduser(args.val_annotations),
+                                        osp.expanduser(args.val_questions),
+                                        args.val_images,
+                                        "val", args)
 
     vocab = vqa_loader.dataset.vocab
     model = DeeperLSTM(len(vocab))
@@ -55,9 +60,11 @@ def main():
     optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
 
     print("Beginning training")
+    print("#"*80)
+
     for epoch in range(args.start_epoch, args.epochs):
         trainer.train(model, vqa_loader, criterion, optimizer, epoch, args)
-        trainer.evaluate(model, val_loader, criterion, epoch)
+        trainer.evaluate(model, val_loader, criterion, epoch, args)
 
     print("Training complete!")
 
