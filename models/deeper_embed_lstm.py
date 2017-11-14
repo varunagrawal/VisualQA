5a5e55dc56f0d7b2e50043ca16b2fcf87fdf2804
Varun Agrawal <varagrawal@gmail.com>
2017-11-14 01:44:11 -0500

    easy model selection

---
+++ models/deeper_embed_lstm.py
@@ -0,0 +1,10 @@
+import torch
+from torch import nn
+
+class DeeperEmbedLSTM(nn.Module):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def forward(self, x):
+        pass
