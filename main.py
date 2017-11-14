508cdba3a051e191199515a5cdb5324cf2815f59
Varun Agrawal <varagrawal@gmail.com>
2017-11-14 01:43:54 -0500

    more options

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os.path as osp
 import argparse
 import dataset
-from models.model import DeeperLSTM
+from models.model import Models
 import trainer
 import visualize
 import torch
@@ -25,17 +25,21 @@ def parse_args():
                         help="Return the question as a list of word IDs so we can use an embedding layer on it")
     parser.add_argument("--top_answer_limit", default=1000, help="The number of answers to consider as viable options")
     parser.add_argument("--max_length", default=25, help="The maximum length to consider to each question")
+    parser.add_argument("--arch", default="DeeperLSTM", help="The model to use for VQA",
+                        choices=tuple([name for name, _ in Models.__members__.items()]))
     parser.add_argument("--epochs", default=50, help="Number of training epochs")
     parser.add_argument("--start_epoch", default=0, help="Starting epoch")
     parser.add_argument('--lr', "--learning_rate", default=3e-4, help="Learning rate")
     parser.add_argument("--lr_decay", default=0.95, help="The learning rate decay")
+    parser.add_argument("--decay_interval", default=1, help="The epoch step size at which to decay the learning rate")
     parser.add_argument("--betas", default=(0.8, 0.999), nargs="+", type=float)
     parser.add_argument("--weight-decay", '--wd', default=0.0, help="Optimizer weight decay")
     parser.add_argument("--print-freq", default=100, help="How frequently to print training stats")
-    parser.add_argument("--batch_size", default=1, help="Batch Size")
+    parser.add_argument("--batch_size", default=16, help="Batch Size")
     parser.add_argument('-j', "--num_workers", default=4)
     parser.add_argument("--save_dir", default="weights", help="Directory where model weights are stored")
     parser.add_argument("--gpu", default=-0, help="The GPU to use for training. For multi-gpu setups.")
+    parser.add_argument("--visualize_freq", default=50, help="Number of iterations after which Visdom should update loss graph")
     parser.add_argument("--port", default=8097, help="The port for the Visdom server")
 
     args = parser.parse_args()
@@ -60,14 +64,15 @@ def main():
                                         args.val_images, "val",
                                         args, vocab=vocab, shuffle=False)
 
-    model = DeeperLSTM(len(vocab))
+    arch = Models[args.arch].value
+    model = arch(len(vocab))
 
     if torch.cuda.is_available():
         model.cuda()
 
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=args.lr, betas=tuple(args.betas), weight_decay=args.weight_decay)
-    scheduler = lr_scheduler.StepLR(optimizer, step_size=5, gamma=args.lr_decay)
+    scheduler = lr_scheduler.StepLR(optimizer, step_size=args.decay_interval, gamma=args.lr_decay)
 
     vis = visualize.Visualizer(args.port)
 
--- trainer.py
+++ trainer.py
@@ -32,11 +32,11 @@ def train(model, dataloader, criterion, optimizer, epoch, args, vis=None):
         loss.backward()
         optimizer.step()
 
-        if vis:
+        if vis and idx % args.visualize_freq == 0:
             vis.update_loss(loss, epoch, idx, len(dataloader), "loss")
 
         if idx > 0 and idx % args.print_freq == 0:
-            print_state(idx, epoch, len(dataloader), avg_loss, avg_acc)
+            print_state(idx, epoch, len(dataloader), avg_loss.avg, avg_acc.avg)
 
     save_checkpoint(model, args, epoch)
 
@@ -66,11 +66,11 @@ def evaluate(model, dataloader, criterion, epoch, args, vis=None):
 
         avg_loss.update(loss.data[0], q.size(0))
 
-        if vis:
+        if vis and idx % args.visualize_freq == 0:
             vis.update_loss(loss, epoch, i, len(dataloader), "val_loss")
 
         if i > 0 and i % args.print_freq == 0:
-            print_state(i, -1, len(dataloader), avg_loss, avg_acc)
+            print_state(i, -1, len(dataloader), avg_loss.avg, avg_acc)
 
 
 def save_checkpoint(model, args, epoch):
@@ -85,7 +85,7 @@ def save_checkpoint(model, args, epoch):
     torch.save(state, osp.join(args.save_dir, filename))
 
 
-def print_state(idx, epoch, size, avg_loss, avg_acc):
+def print_state(idx, epoch, size, loss, acc):
     if epoch >= 0:
         message = "Epoch: [{0}][{1}/{2}]\t\t".format(epoch, idx, size)
     else:
@@ -93,7 +93,7 @@ def print_state(idx, epoch, size, avg_loss, avg_acc):
 
     print(message +
           'Loss {loss:.4f} \t'
-          'Accuracy {acc:.4f}'.format(loss=avg_loss.avg, acc=avg_acc.avg))
+          'Accuracy {acc:.4f}'.format(loss=loss, acc=acc))
 
 
 class AverageMeter(object):
