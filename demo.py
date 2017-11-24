dbcaa8c8974e432b12f6dbf3a3cec6af25380643
Varun Agrawal <varagrawal@gmail.com>
2017-11-24 15:56:47 -0500

    demo ready

--- demo.py
+++ demo.py
@@ -1,9 +1,10 @@
 import argparse
 from models import Models
 from utils import image, text
-from PIL import Image
+from PIL import Image, ImageFont, ImageDraw
 import numpy as np
 import torch
+from torch import nn
 from torch.autograd import Variable
 from torchvision import transforms
 import pickle
@@ -31,17 +32,26 @@ def generate(output, aid_to_ans):
     return ans
 
 
-def main():
-    # TODO Test this
+def display_result(image, question, answer):
+    draw = ImageDraw.Draw(image)
+    draw.text((10, 10), question)
+    draw.text((10, 20), answer)
+    print("{0}: {1}".format(question, answer))
+    image.show()
 
+
+def main():
     args = parse_args()
 
+    print("Loading encoded data...")
     data, vocab, word_to_wid, wid_to_word, ans_to_aid, aid_to_ans = pickle.load(open(args.preprocessed_cache, 'rb'))
 
     # Get VGG model to process the image
     vision_model = image.get_model()
     # Get our VQA model
     model = Models[args.model].value(len(vocab))
+    # The final classifier
+    classifier = nn.Softmax()
 
     weights = torch.load(args.weights)
     model.load_state_dict(weights["model"])
@@ -53,16 +63,25 @@ def main():
     vision_model.eval()
     model.eval()
 
-    with open(args.image, 'rb') as f:
-        img = Image.open(f)
-        img = transforms.ToTensor()(img)
+    img_transforms = transforms.Compose([
+        transforms.Scale((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225])
+    ])
 
+    print("Processing image")
+    im = Image.open(args.image)
+    img = img_transforms(im)
+    img = img.unsqueeze(0)
     img_var = Variable(img)
+
     if torch.cuda.is_available():
         img_var = img_var.cuda()
 
     img_features = vision_model(img_var)
 
+    print("Processing question")
     q = text.process_single_question(args.question, vocab, word_to_wid)
 
     one_hot_vec = np.zeros((len(q["question_wids"]), len(vocab)))
@@ -73,9 +92,16 @@ def main():
     if torch.cuda.is_available():
         q_var = q_var.cuda()
 
-    output = model(img_features, q_var)
-    ans = generate(output, aid_to_ans)
-    print("The answer is: {0}".format(ans))
+    q_var = q_var.unsqueeze(0)
+
+    output = model(img_features, q_var.float())
+    output = classifier(output)
+
+    _, ans_id = torch.max(output.data, dim=1)
+    # index into ans_id since it is a tensor
+    ans = generate(ans_id[0], aid_to_ans)
+
+    display_result(im, args.question, ans)
 
 
 if __name__ == "__main__":
