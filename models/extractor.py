abfd5c545782a2294b70663020ee7e767eae5987
Varun Agrawal <varagrawal@gmail.com>
2018-04-24 18:17:05 -0400

    feature extractor for raw images

---
+++ models/extractor.py
@@ -0,0 +1,12 @@
+from torch import nn
+from utils.image import get_model
+
+
+class FeatureExtractor(nn.Module):
+    def __init__(self, arch="vgg"):
+        super().__init__()
+        self.model, _ = get_model(arch)
+
+    def forward(self, x):
+
+        return self.model(x)
