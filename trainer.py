0862b13f256750ff8a22caf0589a10c379477bc8
Varun Agrawal <varagrawal@gmail.com>
2018-04-25 17:35:44 -0400

    model checkpoint name includes the model architecture

--- trainer.py
+++ trainer.py
@@ -82,7 +82,7 @@ def save_checkpoint(model, args, epoch):
         "model": model.state_dict(),
         "args": args
     }
-    filename = 'vqa_checkpoint_{0}.pth.tar'.format(epoch)
+    filename = 'vqa_checkpoint_{0}_{1}.pth.tar'.format(args.arch, epoch)
     torch.save(state, osp.join(args.save_dir, filename))
 
 
