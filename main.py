bda29be643ae008096624d0794f075a4dd19c628
Varun Agrawal <varagrawal@gmail.com>
2018-04-24 22:08:20 -0400

    bugfix in val transforms to resize image to 224 on both sides

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def main():
         "aid_to_ans": vqa_loader.dataset.aid_to_ans,
     }
     val_transform = transforms.Compose([
-        transforms.Resize(224),
+        transforms.Resize((224, 224)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
                              std=[0.229, 0.224, 0.225])
