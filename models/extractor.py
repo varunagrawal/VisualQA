5682d628896e038e345e9d1e39cb476c2f9bc562
Varun Agrawal <varagrawal@gmail.com>
2018-04-26 13:44:40 -0400

    bug fixes in image feature extractor code

--- models/extractor.py
+++ models/extractor.py
@@ -3,10 +3,9 @@ from utils.image import get_model
 
 
 class FeatureExtractor(nn.Module):
-    def __init__(self, arch="vgg"):
+    def __init__(self, arch="vgg16"):
         super().__init__()
         self.model, _ = get_model(arch)
 
     def forward(self, x):
-
         return self.model(x)
