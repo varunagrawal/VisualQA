5ab34df25e7c44220da95373e5d3699a9593331e
Varun Agrawal <varagrawal@gmail.com>
2018-05-17 18:38:49 -0400

    verified text processing and added nltk as a tokenizer

--- utils/text.py
+++ utils/text.py
@@ -7,29 +7,28 @@ Author: Vaurn Agrawal (varunagrawal)
 import re
 from tqdm import tqdm
 import numpy as np
+import nltk
 
 
-def tokenize(s):
-    t_str = s.lower()
-    for i in [r'\?',r'\!',r'\'',r'\"',r'\$',r'\:',r'\@',r'\(',r'\)',r'\,',r'\.',r'\;']:
-        t_str = re.sub( i, '', t_str)
-    for i in [r'\-',r'\/']:
-        t_str = re.sub( i, ' ', t_str)
-    q_list = re.sub(r'\?','',t_str.lower()).split(' ')
-    q_list = list(filter(lambda x: len(x) > 0, q_list))
-    return q_list
+def tokenize(sentence):
+    sentence = sentence.lower()
+    return [i for i in re.split(r"([-.\"',:? !\$#@~()*&\^%;\[\]/\\\+<>\n=])", sentence) if
+            i != '' and i != ' ' and i != '\n'];
 
 
-def preprocess_questions(dataset, display=True):
+def preprocess_questions(dataset, method="nltk", display=True):
     for idx, d in enumerate(tqdm(dataset, leave=display)):
         s = d["question"]
-        d["question_words"] = tokenize(s)
+        if method == "nltk":
+            d["question_words"] = nltk.word_tokenize(str(s).lower())
+        else:
+            d["question_words"] = tokenize(s)
     return dataset
 
 
-def get_vocabulary(dataset, min_word_count=2):
+def get_vocabulary(dataset, min_word_count=0):
     """
-    Filter out words in the questions that are < min_word_count and create a vocabulary from the filtered words
+    Filter out words in the questions that are <= min_word_count and create a vocabulary from the filtered words
     :param dataset: The VQA dataset
     :param min_word_count: The minimum number of counts the word needs in order to be included
     :return:
@@ -40,10 +39,14 @@ def get_vocabulary(dataset, min_word_count=2):
         for w in d["question_words"]:
             counts[w] = counts.get(w, 0) + 1
 
-    vocab = [w for w, n in counts.items() if n >= min_word_count]
-    # 0 is used for padding
-    vocab.insert(0, '^$')  # special chars to indicate padding
+    vocab = [w for w, n in counts.items() if n > min_word_count]
+
+    # cw = sorted([(n, w) for w, n in counts.items() if n > min_word_count], reverse=True)
+    # print('\n'.join(map(str, cw[:20])))
+
+    # Add the 'UNK' token
     vocab.append('UNK')  # UNK has it's own ID
+
     return vocab
 
 
@@ -73,13 +76,12 @@ def encode_questions(dataset, word_to_wid, max_length=25, display=True):
 
     for idx, d in enumerate(tqdm(dataset, leave=display)):
         d["question_length"] = min(len(d["question_words_UNK"]), max_length)
-        d["question_wids"] = np.zeros(max_length, dtype=np.int32)  # 0 -> UNK
+        d["question_wids"] = np.zeros(max_length, dtype=np.uint32)  # 0 -> UNK
 
         for k, w in enumerate(d["question_words_UNK"]):
             if k < max_length:
                 wid = word_to_wid.get(w, word_to_wid["UNK"])
                 d["question_wids"][k] = int(wid)  # ensure it is an int so it can be used for indexing
-                d['seq_length'] = len(d['question_words_UNK'])  # length of the original sequence
 
     return dataset
 
@@ -99,6 +101,10 @@ def get_top_answers(dataset, top=1000, display=True):
 
     for i in range(top-1):  # the last answer is reserved for out_of_scope
         top_answers.append(ans_counts[i][1])
+
+    if display:
+        print("The top 10 answers are:")
+        print("\n".join(map(str, ans_counts[:10])))
 
     return top_answers
 
