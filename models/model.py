cccdea803f724ea4824bea6ea4840647e99dbcfa
Varun Agrawal <varagrawal@gmail.com>
2018-06-11 11:43:25 -0400

    style fix

--- models/model.py
+++ models/model.py
@@ -1,6 +1,7 @@
 import enum
 from . import deeperlstm, deeper_embed_lstm, mcb
 
+
 class Models(enum.Enum):
     DeeperLSTM = deeperlstm.DeeperLSTM
     DeeperEmbedLSTM = deeper_embed_lstm.DeeperEmbedLSTM
