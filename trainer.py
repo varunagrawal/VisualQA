8f9c590899282b843e5b74f8c8ae1f79963d7b82
Varun Agrawal <varagrawal@gmail.com>
2019-01-08 12:58:42 -0500

    save checkpoints with number of epochs completed

--- trainer.py
+++ trainer.py
@@ -92,7 +92,7 @@ def save_checkpoint(model, args, epoch):
         "model": model.state_dict(),
         "args": args
     }
-    filename = 'vqa_checkpoint_{0}_{1}.pth'.format(args.arch, epoch)
+    filename = 'vqa_checkpoint_{0}_{1}.pth'.format(args.arch, epoch+1)
     torch.save(state, osp.join(args.save_dir, filename))
 
 
