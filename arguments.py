45263315106c1d4c3f42f61352baf6d054d7c781
Varun Agrawal <varagrawal@gmail.com>
2019-01-18 18:20:18 -0500

    cleaned up arguments and added option to save results file

--- arguments.py
+++ arguments.py
@@ -7,23 +7,21 @@ def parse_args():
 
     parser.add_argument("annotations", metavar="TRAIN_ANN",
                         help="The VQA annotations JSON file")
+
     parser.add_argument("questions", metavar="TRAIN_QUES",
                         help="The VQA questions JSON file")
     parser.add_argument("--images", metavar="TRAIN_IMAGES",
-                        help="The file containing either the tensors of the CNN embeddings of COCO images or "
-                             "the json of images")
+                        help="The file containing either the tensors of the CNN embeddings of COCO images or the json of images")
     parser.add_argument("val_annotations", metavar="VAL_ANN",
                         help="The VQA val annotations JSON file")
     parser.add_argument("val_questions", metavar="VAL_QUES",
                         help="The VQA val questions JSON file")
     parser.add_argument("--val_images", metavar="VAL_IMAGES",
-                        help="The file containing either the tensors of the CNN embeddings or the json file of "
-                             "COCO images")
-    parser.add_argument(
-        "--image_root", help="Root path to the images directory")
+                        help="The file containing either the tensors of the CNN embeddings or the json file of COCO images")
+    parser.add_argument("--image_root",
+                        help="Root path to the images directory")
     parser.add_argument("--raw_images", action="store_true", default=False,
-                        help="Flag to indicate if we're using the the raw images instead of the preprocessed "
-                             "embeddings")
+                        help="Flag to indicate if we're using the the raw images instead of the preprocessed embeddings")
     parser.add_argument("--embed_question", action="store_true",
                         help="Return the question as a list of word IDs so we can use an embedding layer on it")
     parser.add_argument("--top_answer_limit", default=1000, type=int,
@@ -59,6 +57,8 @@ def parse_args():
 
     parser.add_argument("--resume", default=None,
                         help="Path to the weights file to resume training from")
+    parser.add_argument("--results_file",
+                        default="VQA_OpenEnded_COCO_results.json")
 
     parser.add_argument("--visualize", action="store_true",
                         help="Flag to indicate use of Visdom for visualization")
